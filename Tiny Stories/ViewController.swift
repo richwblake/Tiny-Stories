206dd15d959b99458de57bce5aedde4146da17d7
wills blake <richwblake@gmail.com>
2022-04-23 20:05:17 -0700

    Initial Commit

---
+++ Tiny Stories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tiny Stories
+//
+//  Created by Wills Blake on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
